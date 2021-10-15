df0799339e1fd65f01a95b9c599b5b01d609643a
officePC <185360986@qq.com>
2021-10-15 17:18:32 +0800

    2015.10.25 daily update

--- src/main/java/com/lan/miaosha/controller/LoginController.java
+++ src/main/java/com/lan/miaosha/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.lan.miaosha.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -10,6 +13,13 @@ public class LoginController {
     public String toLogin(){
         return "login";
     }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public String login(@RequestParam String mobile , @RequestParam String password){
+
+    }
+
 
 
 }
---
+++ src/main/java/com/lan/miaosha/vo/LoginVO.java
@@ -0,0 +1,27 @@
+package com.lan.miaosha.vo;
+
+import com.lan.miaosha.validation.IsMobile;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class LoginVO {
+
+    @NotNull
+    @IsMobile
+    private String mobile;
+
+    @NotNull
+    @Length(min=32)
+    private String password;
+
+    @Override
+    public String toString() {
+        return "LoginVO{" +
+                "mobile='" + mobile + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
